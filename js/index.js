801a61c180f7968efd7823b0efed0b5bdbe2575e
joydharsamrat <joydharsamrat@gmail.com>
2022-09-03 02:17:39 +0600

    display news section added

--- js/index.js
+++ js/index.js
@@ -18,5 +18,59 @@ const displayCategories = categories => {
         catagoriesContainer.appendChild(catagoriesParagraph);
     });
 }
+const loadNews = categoryId => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/${'0' + categoryId}`)
+        .then(res => res.json())
+        .then(data => displayNews(data.data))
+        .catch(error => console.log(error))
+}
 
+const displayNews = newses => {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.textContent = '';
+    const itemsCount = document.getElementById('items-count');
+    itemsCount.innerText = newses.length + ' Items Found';
+    if (newses.length === 0) {
+        newsContainer.innerText = 'Sorry !! No Items Found';
+    }
+    else {
+        newses.forEach(news => {
+            const newsDiv = document.createElement('div');
+            newsDiv.classList.add('card');
+            newsDiv.classList.add('mb-3');
+            newsDiv.innerHTML = `
+                    <div class="row g-0">
+                        <div class="col-md-3">
+                            <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-9">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                </p>
+                                <div class="d-flex justify-content-around align-items-center">
+                                    <div>
+                                        <img src="" alt="">
+                                        <p>x</p>
+                                        <p>y</p>
+                                    </div>
+                                    <div>
+                                        <p>x</p>
+                                    </div>
+                                    <div>
+                                        <p>x</p>
+                                    </div>
+                                    <div>
+                                        <p>x</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+            newsContainer.appendChild(newsDiv);
+        })
+    }
+}
 loadCategories()
